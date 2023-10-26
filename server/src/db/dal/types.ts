a0a30fb820594a33fbfd1b0e769ca46e1c726693
reyraa <ali@muzikie.com>
2023-10-26 12:26:09 +0200

    Create services and corresponding data access layers

---
+++ server/src/db/dal/types.ts
@@ -0,0 +1,9 @@
+export interface GetAllProductsFilters {
+  isDeleted?: boolean
+  includeDeleted?: boolean
+}
+
+export interface GetAllRecipesFilters {
+  isDeleted?: boolean
+  includeDeleted?: boolean
+}
